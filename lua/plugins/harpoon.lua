2fe9d8eb008e38babb58d8ee6dad64de8e60d58a
Goran <gospaso@gmail.com>
2023-11-26 15:08:13 -0400

    add missing keybind descs

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -7,7 +7,7 @@ return {
     local mark = require("harpoon.mark")
     local ui = require("harpoon.ui")
 
-    vim.keymap.set("n", "<leader>m", mark.add_file)
+    vim.keymap.set("n", "<leader>m", mark.add_file, { desc = "Add file to harpoon" })
     vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
     vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
     vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
