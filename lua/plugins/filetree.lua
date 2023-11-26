0273e3abc798b4c41b6ff7e0278c66f9fbc18afd
Goran <gospaso@gmail.com>
2023-11-26 15:08:30 -0400

    add filetree

---
+++ lua/plugins/filetree.lua
@@ -0,0 +1,31 @@
+-- Unless you are still migrating, remove the deprecated commands from v1.x
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require("neo-tree.command").execute({ toggle = true })
+      end,
+      desc = "Explorer NeoTree (root dir)",
+    },
+    {
+      "<leader>E",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+  },
+  config = function()
+    require('neo-tree').setup {}
+  end,
+}
