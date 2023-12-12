389e83effb268e3c11e544bcb489a79537a5bb9f
Goran <gospaso@gmail.com>
2023-12-12 12:32:36 -0400

    remove copilot-cmp

--- init.lua
+++ init.lua
@@ -430,7 +430,6 @@ cmp.setup {
     end, { 'i', 's' }),
   },
   sources = {
-    { name = "copilot",  group_index = 2 },
     { name = 'nvim_lsp', group_index = 2 },
     { name = 'luasnip',  group_index = 2 },
   },
