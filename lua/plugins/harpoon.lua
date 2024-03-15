25005a057ca4d4b98f40f339b1389702b4997e65
Goran <gospaso@gmail.com>
2024-03-15 18:24:33 -0400

    update harpoon bindings

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -12,9 +12,15 @@ return {
     vim.keymap.set("n", "<leader>m", function() harpoon:list():append() end)
     vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-    -- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<M-1>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<M-2>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<M-3>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<M-4>", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<M-5>", function() harpoon:list():select(5) end)
+    vim.keymap.set("n", "<M-6>", function() harpoon:list():select(6) end)
+    vim.keymap.set("n", "<M-7>", function() harpoon:list():select(7) end)
+    vim.keymap.set("n", "<M-8>", function() harpoon:list():select(8) end)
+    vim.keymap.set("n", "<M-9>", function() harpoon:list():select(9) end)
+    vim.keymap.set("n", "<M-0>", function() harpoon:list():select(10) end)
   end,
 }
