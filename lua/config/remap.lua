06ad31ab4d7580bf23913525a3da65897436b80d
Goran <gospaso@gmail.com>
2024-03-15 18:25:53 -0400

    join line without moving cursor

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -3,6 +3,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("n", "J", "mzJ`z")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-down>", "<C-E>", { noremap = true })
